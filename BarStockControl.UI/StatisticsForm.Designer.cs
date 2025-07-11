b134f365d740c022860456f30579a31bcd079c81
gonpampinaicore <89429231+gonpampin@users.noreply.github.com>
2025-07-10 23:01:46 -0300

    raro

--- BarStockControl.UI/StatisticsForm.Designer.cs
+++ BarStockControl.UI/StatisticsForm.Designer.cs
@@ -29,103 +29,124 @@
 
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.mainTableLayout = new System.Windows.Forms.TableLayoutPanel();
-            this.selectorsPanel = new System.Windows.Forms.FlowLayoutPanel();
-            this.chartsTableLayout = new System.Windows.Forms.TableLayoutPanel();
-            this.cboEventos = new System.Windows.Forms.ComboBox();
-            this.cboMeses = new System.Windows.Forms.ComboBox();
-            this.cboTipoBarra = new System.Windows.Forms.ComboBox();
-            this.clbProductos = new System.Windows.Forms.CheckedListBox();
-            this.chartSales = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            this.chartPie = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            this.chartPieEventos = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            this.chartBarras = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            this.chartCostVsSales = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            this.lblTotalVentas = new System.Windows.Forms.Label();
-            this.lblTotalTragos = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.chartSales)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.chartPie)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.chartPieEventos)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.chartBarras)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.chartCostVsSales)).BeginInit();
-            this.SuspendLayout();
+            mainTableLayout = new TableLayoutPanel();
+            selectorsPanel = new FlowLayoutPanel();
+            cboEventos = new ComboBox();
+            cboMeses = new ComboBox();
+            cboTipoBarra = new ComboBox();
+            clbProductos = new CheckedListBox();
+            lblTotalVentas = new Label();
+            lblTotalTragos = new Label();
+            chartsTableLayout = new TableLayoutPanel();
+            mainTableLayout.SuspendLayout();
+            selectorsPanel.SuspendLayout();
+            SuspendLayout();
+            // 
             // mainTableLayout
-            this.mainTableLayout.ColumnCount = 1;
-            this.mainTableLayout.RowCount = 2;
-            this.mainTableLayout.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.mainTableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 130F));
-            this.mainTableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.mainTableLayout.Controls.Add(this.selectorsPanel, 0, 0);
-            this.mainTableLayout.Controls.Add(this.chartsTableLayout, 0, 1);
+            // 
+            mainTableLayout.ColumnCount = 1;
+            mainTableLayout.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
+            mainTableLayout.Controls.Add(selectorsPanel, 0, 0);
+            mainTableLayout.Controls.Add(chartsTableLayout, 0, 1);
+            mainTableLayout.Dock = DockStyle.Fill;
+            mainTableLayout.Location = new Point(0, 0);
+            mainTableLayout.Name = "mainTableLayout";
+            mainTableLayout.RowCount = 2;
+            mainTableLayout.RowStyles.Add(new RowStyle(SizeType.Absolute, 130F));
+            mainTableLayout.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            mainTableLayout.Size = new Size(1200, 900);
+            mainTableLayout.TabIndex = 0;
+            // 
             // selectorsPanel
-            this.selectorsPanel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.selectorsPanel.FlowDirection = System.Windows.Forms.FlowDirection.LeftToRight;
-            this.selectorsPanel.WrapContents = false;
-            this.selectorsPanel.Controls.Add(this.cboEventos);
-            this.selectorsPanel.Controls.Add(this.cboMeses);
-            this.selectorsPanel.Controls.Add(this.cboTipoBarra);
-            this.clbProductos.Width = 250;
-            this.clbProductos.Height = 100;
-            this.selectorsPanel.Controls.Add(this.clbProductos);
-            this.selectorsPanel.Controls.Add(this.lblTotalVentas);
-            this.selectorsPanel.Controls.Add(this.lblTotalTragos);
+            // 
+            selectorsPanel.Controls.Add(cboEventos);
+            selectorsPanel.Controls.Add(cboMeses);
+            selectorsPanel.Controls.Add(cboTipoBarra);
+            selectorsPanel.Controls.Add(clbProductos);
+            selectorsPanel.Controls.Add(lblTotalVentas);
+            selectorsPanel.Controls.Add(lblTotalTragos);
+            selectorsPanel.Dock = DockStyle.Fill;
+            selectorsPanel.Location = new Point(3, 3);
+            selectorsPanel.Name = "selectorsPanel";
+            selectorsPanel.Size = new Size(1194, 124);
+            selectorsPanel.TabIndex = 0;
+            selectorsPanel.WrapContents = false;
+            // 
             // cboEventos
-            this.cboEventos.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cboEventos.Width = 180;
+            // 
+            cboEventos.DropDownStyle = ComboBoxStyle.DropDownList;
+            cboEventos.Location = new Point(3, 3);
+            cboEventos.Name = "cboEventos";
+            cboEventos.Size = new Size(180, 23);
+            cboEventos.TabIndex = 0;
+            // 
             // cboMeses
-            this.cboMeses.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cboMeses.Width = 180;
+            // 
+            cboMeses.DropDownStyle = ComboBoxStyle.DropDownList;
+            cboMeses.Location = new Point(189, 3);
+            cboMeses.Name = "cboMeses";
+            cboMeses.Size = new Size(180, 23);
+            cboMeses.TabIndex = 1;
+            // 
             // cboTipoBarra
-            this.cboTipoBarra.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cboTipoBarra.Width = 180;
+            // 
+            cboTipoBarra.DropDownStyle = ComboBoxStyle.DropDownList;
+            cboTipoBarra.Location = new Point(375, 3);
+            cboTipoBarra.Name = "cboTipoBarra";
+            cboTipoBarra.Size = new Size(180, 23);
+            cboTipoBarra.TabIndex = 2;
+            // 
             // clbProductos
-            this.clbProductos.CheckOnClick = true;
-            this.clbProductos.DisplayMember = "Name";
+            // 
+            clbProductos.CheckOnClick = true;
+            clbProductos.DisplayMember = "Name";
+            clbProductos.Location = new Point(561, 3);
+            clbProductos.Name = "clbProductos";
+            clbProductos.Size = new Size(250, 94);
+            clbProductos.TabIndex = 3;
+            // 
             // lblTotalVentas
-            this.lblTotalVentas.Text = "Total de ventas: $0";
-            this.lblTotalVentas.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.lblTotalVentas.Width = 180;
+            // 
+            lblTotalVentas.Location = new Point(817, 0);
+            lblTotalVentas.Name = "lblTotalVentas";
+            lblTotalVentas.Size = new Size(180, 23);
+            lblTotalVentas.TabIndex = 4;
+            lblTotalVentas.Text = "Total de ventas: $0";
+            lblTotalVentas.TextAlign = ContentAlignment.MiddleLeft;
+            // 
             // lblTotalTragos
-            this.lblTotalTragos.Text = "Total de tragos vendidos: 0";
-            this.lblTotalTragos.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.lblTotalTragos.Width = 200;
+            // 
+            lblTotalTragos.Location = new Point(1003, 0);
+            lblTotalTragos.Name = "lblTotalTragos";
+            lblTotalTragos.Size = new Size(200, 23);
+            lblTotalTragos.TabIndex = 5;
+            lblTotalTragos.Text = "Total de tragos vendidos: 0";
+            lblTotalTragos.TextAlign = ContentAlignment.MiddleLeft;
+            // 
             // chartsTableLayout
-            this.chartsTableLayout.ColumnCount = 2;
-            this.chartsTableLayout.RowCount = 3;
-            this.chartsTableLayout.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.chartsTableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.chartsTableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.chartsTableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33F));
-            this.chartsTableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33F));
-            this.chartsTableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 34F));
-            this.chartsTableLayout.Controls.Add(this.chartSales, 0, 0);
-            this.chartsTableLayout.Controls.Add(this.chartPie, 1, 0);
-            this.chartsTableLayout.Controls.Add(this.chartPieEventos, 0, 1);
-            this.chartsTableLayout.Controls.Add(this.chartBarras, 1, 1);
-            this.chartsTableLayout.Controls.Add(this.chartCostVsSales, 0, 2);
-            this.chartsTableLayout.SetColumnSpan(this.chartCostVsSales, 2);
-            // chartSales
-            this.chartSales.Dock = System.Windows.Forms.DockStyle.Fill;
-            // chartPie
-            this.chartPie.Dock = System.Windows.Forms.DockStyle.Fill;
-            // chartPieEventos
-            this.chartPieEventos.Dock = System.Windows.Forms.DockStyle.Fill;
-            // chartBarras
-            this.chartBarras.Dock = System.Windows.Forms.DockStyle.Fill;
-            // chartCostVsSales
-            this.chartCostVsSales.Dock = System.Windows.Forms.DockStyle.Fill;
+            // 
+            chartsTableLayout.ColumnCount = 2;
+            chartsTableLayout.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            chartsTableLayout.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            chartsTableLayout.Dock = DockStyle.Fill;
+            chartsTableLayout.Location = new Point(3, 133);
+            chartsTableLayout.Name = "chartsTableLayout";
+            chartsTableLayout.RowCount = 3;
+            chartsTableLayout.RowStyles.Add(new RowStyle(SizeType.Percent, 33F));
+            chartsTableLayout.RowStyles.Add(new RowStyle(SizeType.Percent, 33F));
+            chartsTableLayout.RowStyles.Add(new RowStyle(SizeType.Percent, 34F));
+            chartsTableLayout.Size = new Size(1194, 764);
+            chartsTableLayout.TabIndex = 1;
+            // 
             // StatisticsForm
-            this.ClientSize = new System.Drawing.Size(1200, 900);
-            this.Controls.Add(this.mainTableLayout);
-            this.Name = "StatisticsForm";
-            this.Text = "Estadísticas de Ventas";
-            ((System.ComponentModel.ISupportInitialize)(this.chartSales)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.chartPie)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.chartPieEventos)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.chartBarras)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.chartCostVsSales)).EndInit();
-            this.ResumeLayout(false);
+            // 
+            ClientSize = new Size(1200, 900);
+            Controls.Add(mainTableLayout);
+            Name = "StatisticsForm";
+            Text = "Estadísticas de Ventas";
+            mainTableLayout.ResumeLayout(false);
+            selectorsPanel.ResumeLayout(false);
+            ResumeLayout(false);
         }
     }
 }
